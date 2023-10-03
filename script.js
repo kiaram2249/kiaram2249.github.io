3d73edb2c3fa6b712620ecf569879d498f1f164e
kiaram2249 <146884636+kiaram2249@users.noreply.github.com>
2023-10-03 15:58:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Kiara Matamoros"; // update the part BETWEEN the "quotes"
+let userName = "kiaram2249"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
-    e.innerHTML = ownerName;
+    e.innerHTML = Kiara;
 });
 
-document.querySelector("#github").href = "http://github.com/" + userName;
-document.querySelector("#fork").href = "http://github.com/" + userName + "/" + userName + ".github.io";
+document.querySelector("#github").href = "http://github.com/" + kiaram2249;
+document.querySelector("#fork").href = "http://github.com/" + kiaram2249 + "/" + kiaram2249 + ".github.io";
